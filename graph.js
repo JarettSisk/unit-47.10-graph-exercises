0fa6e73f9264255485ac5ac9e2d7b1d3677c406d
Jarett Sisk <jarett.sisk@gmail.com>
2022-05-17 13:54:31 -0500

    finished tasks

---
+++ graph.js
@@ -0,0 +1,86 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for(let vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    this.nodes.delete(vertex);
+    this.nodes.forEach((v) => {
+      if(v.adjacent.has(vertex)) {
+        v.adjacent.delete(vertex);
+      }
+    })
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {
+    let nodeStack = [start];
+    let visitedNodes = new Set(nodeStack);
+    let nodeVals = []
+
+    while(nodeStack.length) {
+      let currentNode = nodeStack.pop();
+      nodeVals.push(currentNode.value);
+      for(let neighbor of currentNode.adjacent) {
+        if(!visitedNodes.has(neighbor)) {
+          nodeStack.push(neighbor);
+          visitedNodes.add(neighbor);
+        }
+      }
+    }
+    return nodeVals;
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+    let nodeStack = [start];
+    let visitedNodes = new Set(nodeStack);
+    let nodeVals = []
+
+    while(nodeStack.length) {
+      let currentNode = nodeStack.shift();
+      nodeVals.push(currentNode.value);
+      for(let neighbor of currentNode.adjacent) {
+        if(!visitedNodes.has(neighbor)) {
+          nodeStack.push(neighbor);
+          visitedNodes.add(neighbor);
+        }
+      }
+    }
+    return nodeVals;
+  }
+}
+
+module.exports = {Graph, Node}
